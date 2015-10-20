f50e23cd31636ee6805e7d16cc0baae71324bb19
icarodgl <icarodgl@gmail.com>
2015-10-20 10:40:38 -0200

    mensagem

--- git.py
+++ git.py
@@ -1,10 +1,14 @@
 import sys
 import os
 def main():
-	os.system("git add .")
-	text = input("mensagem: ")
-	os.system("git commit -m "+ text)
-	os.system("git push")
+	if len(sys.argv) == 2:
+		text = sys.argv[1]
+		os.system("git add .")
+		#text = input("mensagem: ")
+		os.system("git commit -m "+ text)
+		os.system("git push")
+	else:
+		print("argumento MENSAGEM não foi digitado")
 	return 0
 
 if __name__ == '__main__':
