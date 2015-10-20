b329f9b09319f670abb3da7c5337dad5209c1890
icarodgl <icarodgl@gmail.com>
2015-10-20 09:52:19 -0200

    oi

---
+++ git.py
@@ -0,0 +1,12 @@
+import sys
+import os
+def main():
+	os.system("git add .")
+	text = input("mensagem: ")
+	os.system("git commit -m "+ text)
+	os.system("git push")
+	return 0
+
+if __name__ == '__main__':
+	main()
+
