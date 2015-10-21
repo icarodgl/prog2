9af5efe560572457c1f01b89b1719ef870c85373
icarodgl <icarodgl@gmail.com>
2015-10-20 23:13:56 -0200

    tudo

--- autopush.py
+++ autopush.py
@@ -7,15 +7,22 @@
 import sys
 import os
 def main():
-	if len(sys.argv) == 2:
-		text = sys.argv[1]
-		os.system("git add .")
-		#text = input("mensagem: ")
-		os.system("git commit -m "+ text)
-		os.system("git push")
-	else:
-		print("argumento MENSAGEM não foi digitado")
+
+	text = "upado por autopush"
+	os.system("git add .")
+	#text = input("mensagem: ")
+	os.system("git commit -m "+ text)
+	os.system("git push")	
 	return 0
+#	if len(sys.argv) == 2:
+#		text = sys.argv[1]
+#		os.system("git add .")
+		#text = input("mensagem: ")
+#		os.system("git commit -m "+ text)
+#		os.system("git push")
+#	else:
+#		print("argumento MENSAGEM não foi digitado")
+
 
 if __name__ == '__main__':
 	main()
